a05fb952fd2a57cd6b8f0b80588a35d7f41f24d6
Felipe Savoia <felipe.savoia@forescout.com>
2023-10-12 15:05:58 +0200

    creating constant file

---
+++ constants.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Menu(Enum):
+    WELCOME_MSG = """
+    ===================================
+    = Terraform Zero Touch Automation =
+    ===================================
+    """
+    COLOR_MSG = "green"
