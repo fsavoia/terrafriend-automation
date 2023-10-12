36d2b44468587d581303a066a1433759c265d467
Felipe Savoia <felipe.savoia@forescout.com>
2023-10-12 15:06:15 +0200

    first main file with menu call

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Felipe Savoia
+#
+from termcolor import cprint
+
+from constants import Menu
+
+
+def menu_call():
+    cprint(
+        Menu.WELCOME_MSG.value,
+        Menu.COLOR_MSG.value,
+        attrs=["bold"],
+    )
+
+
+def main():
+    menu_call()
+
+
+if __name__ == "__main__":
+    main()
