0e3e6672fe581fa5714daf7ec194758fe55fa1ea
Felipe Savoia <felipe.savoia@forescout.com>
2023-10-17 09:50:43 +0200

    including Copyright in the head

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Felipe Savoia
+#
 from enum import Enum
 
 
--- src/tf_parser.py
+++ src/tf_parser.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Felipe Savoia
+#
 import re
 from dataclasses import dataclass
 
